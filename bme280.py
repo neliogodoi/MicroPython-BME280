a1528c988cb4d03ca9d12b30d47134d4b638074d
Nelio Gonçalves Godoi <neliogodoi@yahoo.com.br>
2018-07-10 08:43:39 -0300

    Update bme280.py

--- bme280.py
+++ bme280.py
@@ -1,27 +1,12 @@
 """
-Authors: Nelio Goncalves Godoi 2017, Roberto Colistete Jr 2017
-Version: 0.1.0 @ 31/03/2018
-
-Based on the work by authors Paul Cunnane 2016, Peter Dahlebrg 2016
-
-Added :
-- diferent oversampling modes for temperature, pressure and humidity.
-  Default values are x2, x16 and x1, respectively;
-- low pass IIR filter settings, when enabled the temperature and pressure
-  resolutions are 20 bits, default value is x16;
-- 3 temperature scales : Celsius, Fahrenheit, Kelvin;
-- wait for sensor data conversion to complete before reading;
-- output format "values" returning 3 floats, with pressure in Pa;
-- output format "formatedValues" returning 3 strings, with pressure in Pa;
-- output format "temperature" returning as a float number;
-- output format "pressure" returning as a float number in Pa;
-- output format "pressurePrecision" returning 2 float numbers, the integer
-  part and decimal part in Pa;
-- output format "humidity" returning as a float number.
-- function "altitude(pressure)" output format "altitude" returning as a float number.
+MicroPython driver for Bosh BME280 temperature, pressure and humidity I2C sensor:
+https://www.bosch-sensortec.com/bst/products/all_products/bme280
+Authors: Nelio Goncalves Godoi, Roberto Colistete Jr
+Version: 3.1.2 @ 2018/04
+License: MIT License (https://opensource.org/licenses/MIT)
 """
 
-from time import sleep, sleep_us
+import time
 from ustruct import unpack, unpack_from
 from array import array
 
@@ -29,41 +14,29 @@ from array import array
 BME280_I2CADDR = 0x76
 # BME280_I2CADDR = 0x77
 
-# OPERATING AND OVERSAMPLING MODES
-OSAMPLE_0 = 0    # Sensor is skipped (output set to 0x80000)
+OSAMPLE_0 = 0
 OSAMPLE_1 = 1
 OSAMPLE_2 = 2
 OSAMPLE_4 = 3
 OSAMPLE_8 = 4
 OSAMPLE_16 = 5
 
-# BME280 REGISTERS
-BME280_REGISTER_STATUS = 0xF3        # Status of sensor data
-BME280_REGISTER_CONTROL_HUM = 0xF2   # Humidity
-BME280_REGISTER_CONTROL = 0xF4       # Temperature and pressure
-BME280_REGISTER_CONTROL_IIR = 0xF5   # Low pass IIR filter
+BME280_REGISTER_STATUS = 0xF3
+BME280_REGISTER_CONTROL_HUM = 0xF2
+BME280_REGISTER_CONTROL = 0xF4
+BME280_REGISTER_CONTROL_IIR = 0xF5
 
-# LOW PASS IIR FILTER SETTINGS
 FILTER_OFF = 0
 FILTER_2 = 1
 FILTER_4 = 2
 FILTER_8 = 3
 FILTER_16 = 4
 
-# TEMPERATURE SCALES/UNITS
 CELSIUS = 'C'
 FAHRENHEIT = 'F'
 KELVIN = 'K'
 
 class BME280(object):
-
-    """ Driver for BME280 sensor of Temperature, Pressure and Humidity """
-    # For default values x2 temperature, x16 pressure and
-    # x1 humidity oversampling, IIR filter x16 :
-    # Maximum measurement time =  t_measured,
-    # max = ((1.25 + (2.3*2) + (2.3*16 + 0.575) + (2.3*1 + 0.575)) ms = 46,1 ms
-    # Filter bandwidth response time = 22 samples
-    # to reach 75% of step response x 46,1 ms = 1.0142 s
 
     def __init__(self,
                  temperature_mode=OSAMPLE_2,
@@ -72,7 +45,7 @@ class BME280(object):
                  temperature_scale=CELSIUS,
                  iir=FILTER_16,
                  address=BME280_I2CADDR,
-                 i2c):
+                 i2c=None):
 
         osamples = [
             OSAMPLE_0,
@@ -83,80 +56,55 @@ class BME280(object):
             OSAMPLE_16]
 
         msg_error = 'Unexpected {} operating mode value {0}.'
-
-        # Check that temperature_mode is valid
         if temperature_mode not in osamples:
             raise ValueError(msg_error.format("temperature", temperature_mode))
         self.temperature_mode = temperature_mode
-
-        # Check that pressure_mode is valid
         if pressure_mode not in osamples:
             raise ValueError(msg_error.format("pressure", pressure_mode))
         self.pressure_mode = pressure_mode
-
-        # Check that humidity_mode is valid
         if humidity_mode not in osamples:
             raise ValueError(msg_error.format("humidity", humidity_mode))
         self.humidity_mode = humidity_mode
-
         msg_error = 'Unexpected low pass IIR filter setting value {0}.'
-
-        # Check that low pass IIR filter setting is valid
         if iir not in [FILTER_OFF, FILTER_2, FILTER_4, FILTER_8, FILTER_16]:
             raise ValueError(msg_error.format(iir))
         self.iir = iir
-
         msg_error = 'Unexpected temperature scale value {0}.'
-
-        # Check that temperature scale valid
         if temperature_scale not in [CELSIUS, FAHRENHEIT, KELVIN]:
             raise ValueError(msg_error.format(temperature_scale))
         self.temperature_scale = temperature_scale
-
         del msg_error
-
         self.address = address
+        if i2c is None:
+            raise ValueError('An I2C object is required.')
         self.i2c = i2c
-
-        # load calibration data
         dig_88_a1 = self.i2c.readfrom_mem(self.address, 0x88, 26)
         dig_e1_e7 = self.i2c.readfrom_mem(self.address, 0xE1, 7)
         self.dig_T1, self.dig_T2, self.dig_T3, self.dig_P1, \
             self.dig_P2, self.dig_P3, self.dig_P4, self.dig_P5, \
             self.dig_P6, self.dig_P7, self.dig_P8, self.dig_P9, \
             _, self.dig_H1 = unpack("<HhhHhhhhhhhhBB", dig_88_a1)
-
         self.dig_H2, self.dig_H3 = unpack("<hB", dig_e1_e7)
         e4_sign = unpack_from("<b", dig_e1_e7, 3)[0]
         self.dig_H4 = (e4_sign << 4) | (dig_e1_e7[4] & 0xF)
-
         e6_sign = unpack_from("<b", dig_e1_e7, 5)[0]
         self.dig_H5 = (e6_sign << 4) | (dig_e1_e7[4] >> 4)
-
         self.dig_H6 = unpack_from("<b", dig_e1_e7, 6)[0]
-
         self.i2c.writeto_mem(
             self.address,
             BME280_REGISTER_CONTROL,
             bytearray([0x24]))
-        # Sleep mode with temperature and pressure with 1x oversampling
-        time.sleep(0.002)   # Needed ?
-
+        time.sleep(0.002)
         self.t_fine = 0
-        # temporary data holders which stay allocated
         self._l1_barray = bytearray(1)
         self._l8_barray = bytearray(8)
         self._l3_resultarray = array("i", [0, 0, 0])
-
-        # Configure low pass IIR filter
         self._l1_barray[0] = self.iir << 2
         self.i2c.writeto_mem(
             self.address,
             BME280_REGISTER_CONTROL_IIR,
             self._l1_barray)
-        time.sleep(0.002)   # Needed ?
-
-        # Configure oversampling of humidity sensor
+        time.sleep(0.002)
         self._l1_barray[0] = self.humidity_mode
         self.i2c.writeto_mem(
             self.address,
@@ -164,9 +112,6 @@ class BME280(object):
             self._l1_barray)
 
     def read_raw_data(self, result):
-        """ Read the raw data of the sensor hardware """
-        # Configure oversampling of pressure and temperature sensors,
-        # enter forced mode (needed for every reading)
         self._l1_barray[0] = (
             self.pressure_mode << 5 |
             self.temperature_mode << 2 | 1)
@@ -190,23 +135,16 @@ class BME280(object):
             sleep_time += 575 + (2300*(1 << self.pressure_mode))
         if self.humidity_mode in osamples_1_16:
             sleep_time += 575 + (2300*(1 << self.humidity_mode))
-        time.sleep_us(sleep_time)  # Wait the required for max measurement time
-
-        # Wait for sensor data conversion to complete
+        time.sleep_us(sleep_time)
         while (unpack('<H',
                       self.i2c.readfrom_mem(
                           self.address,
                           BME280_REGISTER_STATUS, 2))[0] & 0x08):
             time.sleep(0.001)
-
-        # burst readout from 0xF7 to 0xFE, recommended by datasheet
         self.i2c.readfrom_mem_into(self.address, 0xF7, self._l8_barray)
         readout = self._l8_barray
-        # pressure(0xF7): ((msb << 16) | (lsb << 8) | xlsb) >> 4
         raw_press = ((readout[0] << 16) | (readout[1] << 8) | readout[2]) >> 4
-        # temperature(0xFA): ((msb << 16) | (lsb << 8) | xlsb) >> 4
         raw_temp = ((readout[3] << 16) | (readout[4] << 8) | readout[5]) >> 4
-        # humidity(0xFD): (msb << 8) | lsb
         raw_hum = (readout[6] << 8) | readout[7]
 
         result[0] = raw_temp
@@ -217,7 +155,6 @@ class BME280(object):
         """ Get raw data and compensa the same """
         self.read_raw_data(self._l3_resultarray)
         raw_temp, raw_press, raw_hum = self._l3_resultarray
-        # Temperature
         var1 = ((raw_temp >> 3) - (self.dig_T1 << 1)) * (self.dig_T2 >> 11)
 
         var2 = (raw_temp >> 4) - self.dig_T1
@@ -227,7 +164,6 @@ class BME280(object):
         self.t_fine = var1 + var2
         temp = (self.t_fine * 5 + 128) >> 8
 
-        # Pressure
         var1 = self.t_fine - 128000
         var2 = var1 * var1 * self.dig_P6
         var2 = var2 + ((var1 * self.dig_P5) << 17)
@@ -244,7 +180,6 @@ class BME280(object):
             var2 = (self.dig_P8 * p) >> 19
             pressure = ((p + var1 + var2) >> 8) + (self.dig_P7 << 4)
 
-        # Humidity
         h = self.t_fine - 76800
         h = (((((raw_hum << 14) - (self.dig_H4 << 20) -
                 (self.dig_H5 * h)) + 16384)
@@ -266,41 +201,35 @@ class BME280(object):
 
     @property
     def values(self):
-        """ return 3 floats numbers with values of temperature (C or F or K),
-        pressure (Pa), humidity (%) """
         temp, pres, humi = self.read_compensated_data()
-        temp = temp/100              # CELCIUS
+        temp = temp/100
         if self.temperature_scale == 'F':
-            temp = 32 + (temp*1.8)   # FAHRENHEIT
+            temp = 32 + (temp*1.8)
         elif self.temperature_scale == 'K':
-            temp = temp + 273.15     # KELVIN
+            temp = temp + 273.15
 
-        pres = pres/256              # Pa
-        humi = humi/1024             # %
+        pres = pres/256
+        humi = humi/1024
         return (temp, pres, humi)
 
     @property
     def formated_values(self):
-        """ human readable values """
         t, p, h = self.values
         temp = "{} "+self.temperature_scale
         return (temp.format(t), "{} Pa".format(p), "{} %".format(h))
 
     @property
     def temperature(self):
-        """ Return the temperature value """
         t, _, _ = self.values
         return t
 
     @property
     def pressure(self):
-        """ Return the pressure value """
         _, p, _ = self.values
         return p
 
     @property
     def pressure_precision(self):
-        """ Return the pressure integer and decimal values"""
         _, p, _ = self.read_compensated_data()
         pi = float(p // 256)
         pd = (p % 256)/256
@@ -308,12 +237,9 @@ class BME280(object):
 
     @property
     def humidity(self):
-        """ Return the humidity value """
         _, _, h = self.values
         return h
 
     def altitude(self, pressure_sea_level=1013.25):
-        """ Returns the estimated altitude value"""
-        # The International Barometric Formula using hPa.
-        _, p, _ = self.values
-        return 44330*(1-((p/100)/pressure_sea_level)**(1/5.255))
+        pi, pd = self.pressure_precision()
+        return 44330*(1-((float(pi+pd)/100)/pressure_sea_level)**(1/5.255))
